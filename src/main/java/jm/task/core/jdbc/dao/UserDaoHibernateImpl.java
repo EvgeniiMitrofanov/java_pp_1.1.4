af4e5b163ea182dcee5d3815496e1fa8ac5a47db
EvgeniiMitrofanov <johnmitrofanov@gmail.com>
2022-08-24 18:22:41 +0300

    version 1.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.Entity;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -9,34 +16,100 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        Transaction transaction = null;
+        try ( Session session = Util.getConnectionHibernate().openSession())
+        {
+             transaction = session.beginTransaction();
+             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+                     "(id BIGINT PRIMARY KEY AUTO_INCREMENT, nameC VARCHAR(255), lastNameC VARCHAR(255), ageC INT)")
+                     .executeUpdate();
+             transaction.commit();
+             session.close();
+             System.out.println("Table Created Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction transaction = null;
+        try ( Session session = Util.getConnectionHibernate().openSession())
+        {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            transaction.commit();
+            session.close();
+            System.out.println("Table Deleted Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try ( Session session = Util.getConnectionHibernate().openSession())
+        {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+            session.close();
+            System.out.println("Information Added Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try ( Session session = Util.getConnectionHibernate().openSession())
+        {
+            transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+            session.close();
+            System.out.println("Information Deleted Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        Transaction transaction = null;
+        try ( Session session = Util.getConnectionHibernate().openSession())
+        {
+            transaction = session.beginTransaction();
+            list = session.createQuery("From User").list();
+            transaction.commit();
+            session.close();
+            System.out.println("Information Added To List Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try ( Session session = Util.getConnectionHibernate().openSession())
+        {
+            transaction = session.beginTransaction();
+            List<User> list = session.createQuery("From User").list();
+            for (Object o : list) {
+                session.delete(o);
+            }
+            transaction.commit();
+            session.close();
+            System.out.println("Data Deleted Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,6 +13,21 @@ public class Util {
     private static String url = "jdbc:mysql://localhost:3306/mysql";
     private static String password = "кщще";
     private static String user = "root";
+    private static SessionFactory sessionFactory = null;
+    public static SessionFactory getConnectionHibernate() {
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.url", url)
+                    .setProperty("hibernate.connection.username", user)
+                    .setProperty("hibernate.connection.password", password)
+                    .addAnnotatedClass(User.class);
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
     public static Connection getConnection() {
         Connection connection = null;
         try {
